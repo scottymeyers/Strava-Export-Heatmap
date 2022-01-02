b41861cf2f00c62021ab332d0fa9a98a2fc18a62
Scott Meyers <scottmey@gmail.com>
2022-01-01 19:21:08 -0500

    improves Line color UI

--- src/components/PolylineColorPicker.jsx
+++ src/components/PolylineColorPicker.jsx
@@ -3,8 +3,10 @@ import React from 'react';
 const PolylineColorPicker = ({ handleChange, value }) => {
   return (
     <div className="flex justify-space-between">
-      <input onChange={handleChange} type="color" value={value}></input>
-      <div className="color-trans-white">{value}</div>
+      <div className="color-picker" style={{ background: value }}>
+        <input onChange={handleChange} type="color" value={value}></input>
+      </div>
+      <span className="color-trans-white">{value}</span>
     </div>
   );
 };
