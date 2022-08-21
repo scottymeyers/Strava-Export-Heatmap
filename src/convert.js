a64ddf0035bf384150b6f81e27e8bc3dc928c551
Scott Meyers <scottmey@gmail.com>
2022-08-21 15:34:53 -0400

    groups together related GPX functionality

--- src/convert.js
+++ src/convert.js
@@ -6,105 +6,83 @@ const parseString = require('xml2js').parseStringPromise;
 
 const fitParser = new FitParser({ force: true });
 
-/*
-                            /        - opens
-                             \.gz    - matches ".gz" characters
-                                 $   - matches the end of the string
-                                  /  - closes
-                                   i - case insensitive
-  */
-const isGzipped = (path) => /\.gz$/i.test(path);
-/*
-                        /            - opens
-                         \.fit.gz    - matches ".fit.gz" characters
-                                 $   - matches the end of the string
-                                  /  - closes
-                                   i - case insensitive
-  */
-const isFit = (path) => /\.fit.gz$/i.test(path);
+const WRITE_PATH = '../public/activities.json';
+const READ_DIR = '../activities';
 
-const mapActivityType = (type) => {
+const { log } = console;
+
+function mapActivityType(type) {
   switch (type) {
     case 'Cycling': // Element FIT
     case 'Biking': // Strava GPX
     case '1':
       return ['1'];
     case 'Running': // Strava GPX
+    case '9': // Strava GPX
       return ['9'];
     default:
       return [];
   }
-};
+}
 
-const getFilePaths = async (directory) => {
-  const files = await fs.readdir(directory);
+async function getFilePaths() {
+  const files = await fs.readdir(READ_DIR);
   return files.flatMap((file) => {
     const format = file.split('.').pop().toLowerCase();
-    return ['gpx', 'gz'].includes(format) ? `${directory}/${file}` : [];
+    return ['gpx', 'gz'].includes(format) ? `${READ_DIR}/${file}` : [];
   });
-};
-
-const readFile = async (path) => {
-  const fileContent = await fs.readFile(path);
-  if (isGzipped(path)) {
-    if (isFit(path)) {
-      const decompressedFileContent = pako.inflate(fileContent);
-      return new Promise((resolve, reject) => {
-        fitParser.parse(decompressedFileContent, (error, data) => {
-          if (error) reject(error);
-          return resolve(processFitFileData(data));
-        });
-      });
-    } else {
-      const decompressedFileContent = pako.inflate(fileContent, {
-        to: 'string',
-      });
-      return await processGpxFileData(decompressedFileContent);
-    }
-  } else {
-    return await processGpxFileData(fileContent);
-  }
-};
-
-const readFiles = async (paths) => {
-  const data = await Promise.all(paths.map((path) => readFile(path))).then(
-    (fileData) => fileData
-  );
-  const flattened = data.flatMap((d) => d);
-  return JSON.stringify(flattened);
-};
-
-const gpxToJson = async () => {
-  const paths = await getFilePaths('../activities');
-  const json = await readFiles(paths);
-
-  fs.writeFile('../public/activities.json', json, 'utf8', (error) => {
-    if (error) {
-      console.log('An error occured while writing JSON Object to File.');
-      return error;
-    }
-    console.log('JSON file has been saved as ../public/activities.json');
-  });
-};
+}
 
 // GPS Exchange Format
-const processGpxFileData = async (data) => {
+async function processGPX(data) {
   try {
     const parsedString = await parseString(data);
-    if (parsedString) {
-      return parseGpx(parsedString.gpx);
+    if (parsedString && parsedString.gpx) {
+      const { gpx } = parsedString;
+      if (!gpx.trk) return [];
+
+      const parsedTracks = [];
+
+      gpx.trk.forEach((trk) => {
+        const name = trk.name && trk.name.length > 0 ? trk.name[0] : 'untitled';
+        log(`Processing ${name}`);
+        trk.trkseg.forEach((trkseg) => {
+          let points = [];
+          let timestamp;
+          for (let trkpt of trkseg.trkpt || []) {
+            if (trkpt.time && typeof trkpt.time[0] === 'string') {
+              timestamp = new Date(trkpt.time[0]);
+            }
+            if (trkpt.$?.lat && trkpt.$?.lon) {
+              points.push({
+                lat: parseFloat(trkpt.$.lat),
+                lng: parseFloat(trkpt.$.lon),
+              });
+            }
+          }
+          if (points.length > 0) {
+            parsedTracks.push({
+              name,
+              timestamp,
+              type: mapActivityType(trk.type[0]),
+              points,
+            });
+          }
+        });
+      });
+
+      return parsedTracks;
     }
     return [];
   } catch (error) {
-    console.log('!', error);
+    log(error);
   }
-};
+}
 
 // Flexible and Interoperable Data Transfer (FIT) protocol
-const processFitFileData = (data) => {
+function processFIT(data) {
   const name = data.file_ids?.[0]?.manufacturer;
-  console.log(`Processing ${name}`);
-
+  log(`Processing ${name}`);
   return {
     name,
     points: data.records.flatMap((record) => {
@@ -119,43 +97,36 @@ const processFitFileData = (data) => {
     timestamp: data.activity?.timestamp,
     type: mapActivityType(data.workout.wkt_name),
   };
-};
+}
 
-const parseGpx = (gpx) => {
-  if (!gpx.trk) return [];
-
-  const parsedTracks = [];
-
-  gpx.trk.forEach((trk) => {
-    const name = trk.name && trk.name.length > 0 ? trk.name[0] : 'untitled';
-    console.log(`Processing ${name}`);
-
-    trk.trkseg.forEach((trkseg) => {
-      let points = [];
-      let timestamp;
-      for (let trkpt of trkseg.trkpt || []) {
-        if (trkpt.time && typeof trkpt.time[0] === 'string') {
-          timestamp = new Date(trkpt.time[0]);
-        }
-        if (trkpt.$?.lat && trkpt.$?.lon) {
-          points.push({
-            lat: parseFloat(trkpt.$.lat),
-            lng: parseFloat(trkpt.$.lon),
-          });
-        }
-      }
-      if (points.length > 0) {
-        parsedTracks.push({
-          name,
-          timestamp,
-          type: mapActivityType(trk.type[0]),
-          points,
+async function readFile(path) {
+  const fileContent = await fs.readFile(path);
+  if (/\.gz$/i.test(path)) {
+    if (/\.fit.gz$/i.test(path)) {
+      const decompressedFileContent = pako.inflate(fileContent);
+      return new Promise((resolve, reject) => {
+        fitParser.parse(decompressedFileContent, (error, data) => {
+          if (error) reject(error);
+          return resolve(processFIT(data));
         });
-      }
-    });
+      });
+    } else {
+      const decompressedFileContent = pako.inflate(fileContent, {
+        to: 'string',
+      });
+      return await processGPX(decompressedFileContent);
+    }
+  } else {
+    return await processGPX(fileContent);
+  }
+}
+
+(async () => {
+  const paths = await getFilePaths();
+  const data = await Promise.all(paths.map((path) => readFile(path)));
+  const json = JSON.stringify(data.flatMap((d) => d));
+
+  fs.writeFile(WRITE_PATH, json, { encoding: 'utf8' }).then(() => {
+    log(`JSON file has been saved to ${WRITE_PATH.replace('../', '')}`);
   });
-
-  return parsedTracks;
-};
-
-gpxToJson();
+})();
