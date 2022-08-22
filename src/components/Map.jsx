a7d461573f5c6e83a6238b56cb2d2bcbec7290d9
Scott Meyers <scottmey@gmail.com>
2022-08-21 20:04:58 -0400

    messes around with zustand

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,46 +1,57 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef } from 'react';
 import ReactDOM from 'react-dom';
 import { divIcon } from 'leaflet';
 import { MapContainer, Marker, Polyline, TileLayer } from 'react-leaflet';
+import create from 'zustand';
+import shallow from 'zustand/shallow';
 import debounce from 'lodash.debounce';
 
 import Widgets from './Widgets';
 
-const Map = () => {
-  const mapRef = useRef(null);
-
-  const [activities, setActivities] = useState([]);
-  const [activityType, setActivityType] = useState('1');
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [mapCenter, setMapCenter] = useState({
+const useMapStore = create((set) => ({
+  activities: [],
+  activityType: '1',
+  center: {
     lat: 40.73061,
     lng: -73.935242,
-  });
-  const [lineOptions, setLineOptions] = useState({
-    color: '#ff69bf',
+  },
+  errorMessage: null,
+  lineOptions: {
+    color: '#37ff37',
     dashArray: '0',
     dashOffset: '0',
     opacity: 0.5,
     weight: 1,
-  });
-  const [userLocation, setUserLocation] = useState(null);
+  },
+  userLocation: null,
+  setActivityType: (activityType) => set({ activityType }),
+  setActivities: (activities) => set({ activities }),
+  setCenter: (center) => set({ center }),
+  setErrorMessage: (errorMessage) => set({ errorMessage }),
+  setLineOptions: (lineOptions) => set({ lineOptions }),
+  setUserLocation: (userLocation) => set({ userLocation }),
+}));
+
+const Map = () => {
+  const mapRef = useRef(null);
+
+  const store = useMapStore((state) => state);
 
   const filteredActivities = useMemo(
     () =>
-      activities.map((activity) => {
+      store.activities.map((activity) => {
         return {
-          color:
-            activityType === '0' || activityType === activity.type[0]
-              ? lineOptions.color
-              : 'transparent',
-          opacity: lineOptions.opacity,
-          dashArray: lineOptions.dashArray,
-          dashOffset: lineOptions.dashOffset,
-          weight: lineOptions.weight,
+          color: ['0', activity.type[0]].includes(store.activityType)
+            ? store.lineOptions.color
+            : 'transparent',
+          opacity: store.lineOptions.opacity,
+          dashArray: store.lineOptions.dashArray,
+          dashOffset: store.lineOptions.dashOffset,
+          weight: store.lineOptions.weight,
           ...activity,
         };
       }),
-    [activities, activityType, lineOptions]
+    [store.activities, store.activityType, store.lineOptions]
   );
 
   const handleLineChange = (event) => {
@@ -49,19 +60,19 @@ const Map = () => {
     debounce(() => {
       switch (id) {
         case 'lineColor':
-          setLineOptions((o) => ({ ...o, color: value }));
+          store.setLineOptions({ ...store.lineOptions, color: value });
           break;
         case 'lineDashArray':
-          setLineOptions((o) => ({ ...o, dashArray: value }));
+          store.setLineOptions({ ...store.lineOptions, dashArray: value });
           break;
         case 'lineDashOffset':
-          setLineOptions((o) => ({ ...o, dashOffset: value }));
+          store.setLineOptions({ ...store.lineOptions, dashOffset: value });
           break;
         case 'lineOpacity':
-          setLineOptions((o) => ({ ...o, opacity: value }));
+          store.setLineOptions({ ...store.lineOptions, opacity: value });
           break;
         case 'lineWeight':
-          setLineOptions((o) => ({ ...o, weight: value }));
+          store.setLineOptions({ ...store.lineOptions, weight: value });
           break;
         default:
           break;
@@ -80,10 +91,10 @@ const Map = () => {
         }
       })
       .then((data) => {
-        setActivities(data);
+        store.setActivities(data);
       })
       .catch((error) => {
-        setErrorMessage(error.message);
+        store.setErrorMessage(error.message);
       });
   }, []);
 
@@ -94,7 +105,7 @@ const Map = () => {
     } else {
       navigator.geolocation.getCurrentPosition((currentPosition) => {
         const { latitude, longitude } = currentPosition.coords;
-        setUserLocation({ lat: latitude, lng: longitude });
+        store.setUserLocation({ lat: latitude, lng: longitude });
       });
     }
   }, []);
@@ -102,37 +113,39 @@ const Map = () => {
   // keep track of map center coordinates
   useEffect(() => {
     mapRef?.current?.on('moveend', () => {
-      setMapCenter(mapRef.current.getCenter());
+      store.setCenter(mapRef.current.getCenter());
     });
   }, [mapRef.current]);
 
   return (
     <>
-      {activities.length === 0 && (
+      {store.activities.length === 0 && (
         <div className="loading">
-          <span>{errorMessage ? errorMessage : 'Loading activities...'}</span>
+          <span>
+            {store.errorMessage ? store.errorMessage : 'Loading activities...'}
+          </span>
         </div>
       )}
       <>
         <Widgets
-          activityType={activityType}
+          activityType={store.activityType}
           handleLineChange={handleLineChange}
           handleActivitySelect={(e) => setActivityType(e.target.value)}
-          lineOptions={lineOptions}
-          mapCenter={mapCenter}
+          lineOptions={store.lineOptions}
+          mapCenter={store.center}
           mapRef={mapRef}
-          userLocation={userLocation}
+          userLocation={store.userLocation}
         />
         <button
           className="button mobile-locate"
-          disabled={!userLocation}
-          onClick={() => mapRef.current.panTo(userLocation)}
+          disabled={!store.userLocation}
+          onClick={() => mapRef.current.panTo(store.userLocation)}
           type="button"
         >
           &#10166;
         </button>
         <MapContainer
-          center={[mapCenter.lat, mapCenter.lng]}
+          center={[store.center.lat, store.center.lng]}
           className="map"
           scrollWheelZoom={false}
           whenCreated={(map) => (mapRef.current = map)}
@@ -157,9 +170,9 @@ const Map = () => {
               smoothFactor={1}
             />
           ))}
-          {userLocation && (
+          {store.userLocation && (
             <Marker
-              position={userLocation}
+              position={store.userLocation}
               icon={divIcon({
                 className: '',
                 html: `
@@ -182,4 +195,5 @@ const Map = () => {
   );
 };
 
+ReactDOM.render(<Map />, document.getElementById('root'));
 ReactDOM.render(<Map />, document.getElementById('root'));
